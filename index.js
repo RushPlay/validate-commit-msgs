1014b65a545f30fd87f5380c37fa50866381ee6f
Leonard Kinday <leonard@kinday.ru>
2017-03-08 16:39:35 +0100

    feat: add `--base` flag

--- index.js
+++ index.js
@@ -2,16 +2,34 @@
 
 const { exec } = require('child_process')
 const chalk = require('chalk')
+const meow = require('meow')
 const validateMessage = require('validate-commit-msg')
 
 if (require.main === module) {
+  const cli = meow(`
+    Usage
+      $ validate-commit-msgs
+
+    Options
+      --base, -b  Base branch to compare with. Default is 'master'.
+
+    Examples
+      $ validate-commit-msgs
+      $ validate-commit-msgs -b origin/master
+  `, {
+    alias: {
+      b: 'base'
+    }
+  })
+
   exec('git rev-parse --abbrev-ref HEAD', (err, branch) => {
     if (err) {
       throw err
     }
 
     if (branch.trim() !== 'master') {
-      exec('git log --pretty=format:%s master..HEAD', (err, result) => {
+      const base = cli.flags.base || 'master'
+      exec(`git log --pretty=format:%s ${base}..HEAD`, (err, result) => {
         if (err) {
           throw err
         }
